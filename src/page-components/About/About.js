896b5e32c387a634061d16b0adc2670a8b0e8096
Dosaki <lord.g.bug@gmail.com>
2022-08-25 21:59:51 +0100

    a word :/

--- src/page-components/About/About.js
+++ src/page-components/About/About.js
@@ -43,7 +43,7 @@ class About extends React.Component {
         <div className="body">
           <div className="text">
             <div>I'm a software developer from Portugal. Currently living in the UK.</div>
-            <div>I work <a href="https://keyholding.com/">The Keyholding Company</a> as the Lead Backend Developer.</div>
+            <div>I work at <a href="https://keyholding.com/">The Keyholding Company</a> as the Lead Backend Developer.</div>
             <div>Before, I worked as the Software Development Manager at <a href="https://www.panintelligence.com/">Panintelligence</a>, where I got to play with data and mentor our devs.</div>
             <div>Online, I use '<span className="highlight">Dosaki</span>' as my monicker.</div>
             <div className="spacer"></div>
