2c212717bedf4e620219af3f01e1d68a742d39a5
Tiago Correia <tiago.f.a.correia@gmail.com>
2024-08-11 13:27:29 +0100

    Adding terraform deployment

--- src/page-components/Projects/Projects.js
+++ src/page-components/Projects/Projects.js
@@ -1,22 +1,36 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './Projects.css';
-import { v4 as uuid } from 'uuid';
+import {v4 as uuid} from 'uuid';
 import Project from '../../components/Project/Project';
-import projects from '../../data/projects.json'
+import axios from 'axios';
 
-class Projects extends React.Component {
-  render() {
-    return (
-      <div className="Projects">
+function Projects() {
+    const [projects, setProjects] = useState([]);
+    useEffect(() => {
+        const getProjects = async () => {
+            const response = await axios.get('https://dosaki-public-dist.s3.eu-west-1.amazonaws.com/dosaki.net/config/projects.json');
+            setProjects(response.data);
+        };
+        getProjects()
+    }, []);
+    return <div className="Projects">
         <h1>Stuff I make</h1>
         <ul className="list">
-          {projects.filter(p=>p.status === "active").sort((a,b)=>a.type > b.type ? 1 : a.type < b.type ? -1 : 0).map(p => <Project key={uuid()} detail={p} />)}
-          {projects.filter(p=>p.status === "done").sort((a,b)=>a.type > b.type ? 1 : a.type < b.type ? -1 : 0).map(p => <Project key={uuid()} detail={p} />)}
-          {projects.filter(p=>!["active", "done"].includes(p.status)).sort((a,b)=>a.type > b.type ? 1 : a.type < b.type ? -1 : 0).map(p => <Project key={uuid()} detail={p} />)}
+            {projects
+            .filter(p => p.status === 'active')
+            .sort((a, b) => a.type > b.type ? 1 : a.type < b.type ? -1 : 0)
+            .map(p => <Project key={uuid()} detail={p}/>)}
+            {projects
+            .filter(p => p.status === 'done')
+            .sort((a, b) => a.type > b.type ? 1 : a.type < b.type ? -1 : 0)
+            .map(p => <Project key={uuid()} detail={p}/>)}
+            {projects
+            .filter(p => !['active', 'done']
+            .includes(p.status))
+            .sort((a, b) => a.type > b.type ? 1 : a.type < b.type ? -1 : 0)
+            .map(p => <Project key={uuid()} detail={p}/>)}
         </ul>
-      </div>
-    );
-  }
+    </div>;
 }
 
 export default Projects;
--- src/page-components/Talks/Talks.js
+++ src/page-components/Talks/Talks.js
@@ -1,20 +1,26 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './Talks.css';
-import { v4 as uuid } from 'uuid';
-import talks from '../../data/talks.json'
+import {v4 as uuid} from 'uuid';
 import Talk from '../../components/Talk/Talk';
+import axios from 'axios';
 
-class Talks extends React.Component {
-  render() {
-    return (
-      <div className="Talks">
+function Talks() {
+    const [talks, setTalks] = useState([]);
+    useEffect(() => {
+        const getTalks = async () => {
+            const response = await axios.get('https://dosaki-public-dist.s3.eu-west-1.amazonaws.com/dosaki.net/config/talks.json');
+            setTalks(response.data);
+        };
+        getTalks();
+    }, []);
+
+    return <div className="Talks">
         <h1>Stuff I talk about</h1>
         <ul className="list">
-          {talks.sort((a,b)=>a.date > b.date ? -1 : a.date < b.date ? 1 : 0).map(p => <Talk key={uuid()} detail={p} />)}
+            {talks.sort((a, b) => a.date > b.date ? -1 : a.date < b.date ? 1 : 0).map(p => <Talk key={uuid()}
+                                                                                                 detail={p}/>)}
         </ul>
-      </div>
-    );
-  }
+    </div>;
 }
 
 export default Talks;
